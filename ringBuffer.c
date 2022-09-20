3b9b816440c52b46d6da0cec3815454eb7d90bda
Levi Jordan <levi@terradepth.com>
2022-09-20 16:17:21 -0500

    populated defintions for interface, removed some unnecessary functions in API

--- ringBuffer.c
+++ ringBuffer.c
@@ -1,4 +1,103 @@
-#include <limits> //LLONG_MAX, INT_MAX can vary between machines
-
 #include "ringBuffer.h"
 
+struct ringBuffer {
+    int* data;
+    size_t head;
+    size_t tail;
+    int capacity;
+    bool full;
+};
+
+ringBufferHandle initRingBuffer(int* data, int capacity)
+{
+    //verify memory and size
+    assert(buffer && capacity);
+
+    ringBufferHandle ringBuff = malloc(sizeof(ringBuffer));
+    
+    //verify memory allocation
+    assert(ringBuff);
+
+    ringBuff->data = data;
+
+    ringBuff->capacity = capacity;
+
+    clearRingBuffer(ringBuff);
+
+    return ringBuff;
+}
+
+void clearRingBuffer(ringBufferHandle buffer)
+{
+    assert(buffer);
+
+    buffer->full = false;
+    buffer->head = 0;
+    buffer->tail = 0;
+
+    //verify the empty actually worked
+    assert(isEmpty(buffer));
+}
+
+bool isEmpty(ringBufferHandle buffer)
+{
+    assert(buffer);
+
+    return (buffer-> head == buffer-> tail) && (!buffer->full);
+}
+
+size_t size(ringBufferHandle buffer)
+{
+    assert(buffer);
+
+    if (buffer->full)
+    {
+        return buffer->capacity;
+    }
+    else
+    {
+        return (buffer->head) - (buffer->tail);
+    }
+}
+
+void push(ringBufferHandle buffer, int newValue)
+{
+    assert(buffer & buffer->data && newValue);
+
+    buffer->data[buffer->head] = newValue;
+
+    //if the buffer was already full, tail needs to move
+    if (buffer->full)
+    {
+        if (++(buffer->tail) == buffer->capacity)
+        {
+            buffer->tail = 0;
+        }
+    }
+
+    //update head and check if the add filled the buffer
+    if(++(buffer->head) == buffer->capacity)
+    {
+        //reset the head
+        buffer->head = 0;
+
+        //update full status
+        buffer->full = (buffer->head) == (buffer->tail);
+    }
+}
+
+float avg(ringBufferHandle buffer)
+{
+    assert(buffer && buffer->data)
+    
+    //long long to prevent overflow
+    long long runningSum = 0;
+
+    for (int i = 0; i < size(buffer); i++)
+    {
+        //doesn't matter if buffer has wrapped, math is the same
+        runningSum += buffer->data[i];
+    }
+
+    return (float)runningSum / size;
+}
--- ringBuffer.h
+++ ringBuffer.h
@@ -1,6 +1,11 @@
 /** @file ringBuffer.h
  * 
  * @brief A ring buffer to store integers, and some useful functions
+ *          implementated with design by contract, will fail hard if 
+ *          misused.
+ * 
+ *          use-case specific assumption: values are never removed
+ *              once full, the buffer remains full for its lifespan
  *
  * @par       
  * COPYRIGHT NOTICE: (header template) (c) 2018 Barr Group.  All rights reserved.
@@ -24,7 +29,7 @@ typedef ringBuffer* ringBufferHandle;
  * 
  * @return handle to the newly created ringBuffer
  */
-ringBufferHandle initRingBuffer(int* buffer, int capacity);
+ringBufferHandle initRingBuffer(int* data, int capacity);
 
 /* -------------------------------------------------------------------------- */
 
@@ -39,24 +44,13 @@ void push(ringBufferHandle buffer, int newValue);
 /* -------------------------------------------------------------------------- */
 
 /**
- * @brief add all elements of the ring buffer together
- * 
- * @param[in] buffer ring buffer to sum
- * 
- * @return sum of all elements. long long to prevent overflow when summing
- */
-long long sum(ringBufferHandle buffer);
-
-/* -------------------------------------------------------------------------- */
-
-/**
  * @brief get the average value of the buffer
  * 
  * @param[in] buffer ring buffer to avg
  * 
- * @return average buffer value, double to minimize rounding error
+ * @return average buffer value, float to minimize rounding error
  */
-double avg(ringBufferHandle buffer);
+float avg(ringBufferHandle buffer);
 
 /* -------------------------------------------------------------------------- */
 
@@ -65,7 +59,7 @@ double avg(ringBufferHandle buffer);
  * 
  * @param[out] buffer ringBuffer to clear
  */
-void clearRingBuffer(ringBufferHandle buffer);
+bool clearRingBuffer(ringBufferHandle buffer);
 
 /* -------------------------------------------------------------------------- */
 
@@ -76,18 +70,7 @@ void clearRingBuffer(ringBufferHandle buffer);
  * 
  * @return 1 if empty, 0 if not empty
  */
-bool isEmpty(ringBufferHandle bufferHandle);
-
-/* -------------------------------------------------------------------------- */
-
-/**
- * @brief Determine if the ring buffer is full or not
- * 
- * @param[in] handle to ring buffer
- * 
- * @return 1 if full, 0 if not full
- */
-bool isFull(ringBufferHandle bufferHandle);
+bool isEmpty(ringBufferHandle buffer);
 
 /* -------------------------------------------------------------------------- */
 
@@ -98,7 +81,7 @@ bool isFull(ringBufferHandle bufferHandle);
  * 
  * @return size of the buffer in bytes
  */
-size_t size(ringBufferHandle bufferHandle);
+size_t size(ringBufferHandle buffer);
 
 /* -------------------------------------------------------------------------- */
 
